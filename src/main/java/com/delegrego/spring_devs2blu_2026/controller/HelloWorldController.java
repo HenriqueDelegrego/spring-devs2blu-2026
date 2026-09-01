f5debf245cd0c196391f19bc58cdb48c6c39d8a6
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2026-08-31 21:23:35 -0300

    Adapta controller a utilizar service

--- src/main/java/com/delegrego/spring_devs2blu_2026/controller/HelloWorldController.java
+++ src/main/java/com/delegrego/spring_devs2blu_2026/controller/HelloWorldController.java
@@ -1,15 +1,25 @@
 package com.delegrego.spring_devs2blu_2026.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.delegrego.spring_devs2blu_2026.dto.HelloWorldRequisicao;
+import com.delegrego.spring_devs2blu_2026.dto.HelloWorldResposta;
+import com.delegrego.spring_devs2blu_2026.service.HelloWorldService;
 
 @RestController
 public class HelloWorldController {
 
-	@GetMapping("/hello/{nome}/{idade}")
-	public String helloWorld(@PathVariable String nome, @PathVariable int idade) {
-		return "Hello, " + nome + ", você tem " + idade + " anos";
+	@Autowired
+	HelloWorldService servico;
+
+	@PostMapping("/hello")
+	public HelloWorldResposta helloWorld(@RequestBody HelloWorldRequisicao requisicao) {
+		return servico.helloWorld(requisicao);
 	}
 
 	@GetMapping("/tchau")
